a45113e83bf1cc0acb499feee9769c9cec772920
Schonecker, Brian <bschonecker@healthplan.com>
2015-12-15 10:55:57 -0500

    Renamed parameter to be consistent with other wrapper classes.

--- manifests/sudo/sudo.pp
+++ manifests/sudo/sudo.pp
@@ -1,4 +1,4 @@
-class wrappers::sudo::sudo ($sudoer=Undef,)
+class wrappers::sudo::sudo ($resources=Undef,)
 {
-  create_resources(sudo::sudoers, $sudoer)
+  create_resources(sudo::sudoers, $resources)
 }
